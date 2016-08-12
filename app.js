fe1950c2db9b2fbdfdc6d2dcadffa512c6315f13
Sven Hecht <sven.hecht@citrix.com>
2016-08-12 19:34:10 +0200

    added cors

--- app.js
+++ app.js
@@ -26,6 +26,12 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(function(err, req, res, next) {
   res.sendStatus(err.status || 500);
 });
